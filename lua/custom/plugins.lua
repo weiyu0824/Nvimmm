712bddf6c461822b5bb7c813ed5ea29cb147387b
Wei-Yu <jcab1688@gmail.com>
2024-06-09 01:07:44 -0500

    Add c++/python lsp

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -1,4 +1,19 @@
 local plugins = {
-
+  {
+    "neovim/nvim-lspconfig",
+    config = function ()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "clangd",  -- C++
+        "pyright", -- Python
+      }
+    }
+  }
 }
 return plugins
