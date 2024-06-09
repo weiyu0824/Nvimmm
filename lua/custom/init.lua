e1db910c3c5c606c3e5858313c162b7fa248fb13
wylin2 <wylin2@node-1.weiyu2.gaea-pg0.wisc.cloudlab.us>
2024-06-09 07:47:08 +0000

    Allow copy/paste to system clipboard

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -1,4 +1,18 @@
 -- Custom configuration file for Vim
 
--- Map 'kj' to <ESC> in insert mode
+-- Bind 'kj' to <ESC> in insert mode
 vim.api.nvim_set_keymap('i', 'kj', '<Esc>', { noremap = true, silent = true })
+
+-- Set clipboard provider
+vim.g.clipboard = {
+  name = 'OSC 52',
+  copy = {
+    ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+  },
+  paste = {
+    ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+  },
+}
+
